f53d260de98a01a188e3dd78166fa64a3b4804fd
连志明 <lianzhiming@udesk.cn>
2018-12-27 17:42:56 +0800

    沁荷初始化一个项目

---
+++ src/common/locales/zh-cn.js
@@ -0,0 +1,75 @@
+var locale = {
+    labels: {
+        all: "全部",
+        confirm: "确定",
+        colon: ": ",
+        question: "问题",
+        answer: "答案",
+        to:"至",
+        versionUpdate: "很高兴的通知您，我们网站刚刚发布了新版本！为了保证您的正常使用，我们需要您立即保存当前的工作，然后刷新页面（点击浏览器的刷新按钮，或者按下F5键），以让我们的新版本生效。非常感谢您的配合！",
+    },
+    business: {
+        common: {
+
+        },
+        notifyMessage: {
+            IsNullErrorFormat: "请至少有一个一个{0}",
+            canNotBeEmptyFormat: "{0}不能为空",
+            repeatErrorTipFormat: "{0}有重复",
+            getInformationFailedFormat: "获取{0}信息失败，请检查网络连接或稍后再试",
+            deleteSuccessFormat: "{0}删除成功。",
+            deleteFailedFormat: "{0}删除失败，请稍后再试。",
+            saveSuccessFormat: "{0}保存成功。",
+            saveFailedFormat: "{0}保存失败，请稍后再试。",
+            exportSuccessFormat: "{0}导出成功",
+            exportFailedFormat: "{0}导出失败，请稍后再试。",
+            submitSuccessFormat: "{0}提交成功。",
+            submitFailedFormat: "{0}提交失败，请稍后重试。",
+            confirmSuccessFormat: "{0}确认成功。",
+            confirmFailedFormat: "{0}确认失败，请稍后重试。",
+        },
+    },
+    components: {
+        intervalValue: {
+            greaterThan: "大于",
+            lessThan: "小于",
+            to:"-",
+        },
+    },
+    pages: {
+        intelligentKnowledgeDiscovery: {
+            list: {
+                title: "智能知识发现",
+                tipOne: "使用Ubot，一键转为机器人知识库",
+                tipTwo: "为您自动处理12313次咨询，将客服效率提高100%。",
+                hotline: "采购热线： 010-86461073",
+            },
+            detail: {
+                title:"知识详情",
+                bestAnswerTitle: "可能的最佳问答",
+                allAnswersFormat:"所有问答({0})",
+                discoveryTime: "发现时间",
+                dialogueRecords:"对话记录",
+            },
+        },
+    },
+    enums: {
+        intelligentListFilterConditionTypes: {
+            firstDiscovery: "最早发现",
+            recentDiscovery: "最近发现",
+            aggregationNumber: "聚合数量",
+        }
+    },
+    validation: {
+        requiredFormat: "{0}不能为空",
+        required: "此项不能为空",
+        wrongFormat: "{0}格式错误",
+    },
+    ui: {
+        widgets: {
+
+        },
+    },
+};
+
+export default locale;
---
+++ src/pages/app/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.scss';
+import './index.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        你好
+      </div>
+    );
+  }
+}
+
+export default App;
